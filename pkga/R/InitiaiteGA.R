5c4c9b82012561dba192690b36e791c62901423f
Mohamed Ismail <mhismail@buffalo.edu>
2018-03-11 20:18:42 -0400

    Bug fix

--- pkga/R/InitiaiteGA.R
+++ pkga/R/InitiaiteGA.R
@@ -51,7 +51,9 @@ NextGA <- function (alltokens,control=NULL,allmods=NULL){
   b<-crossover(a,2)
   c <- mutation(b,alltokens)
   
-  modlookup <- read.csv("Allmodsresults.csv",as.is=T)[-2:-9]
+  zz=gzfile('Allmodsresults.csv.gz')   
+  allmodsresults <- read.csv(zz)
+  modlookup <- allmodsresults[-2:-9]
   names(modlookup)[1]<- "X"
   print(head(modlookup))
   newmodnumbers <- merge(modlookup,c)$X
